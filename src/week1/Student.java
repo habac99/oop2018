5b36b5c6be180f5285e8300f3ef789a7c0b5d0ec
Hà Văn Bắc <havanbac99@gmail.om>
2018-09-12 10:26:09 +0700

    test

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -5,12 +5,41 @@ public class Student {
     // TODO: khai báo các thuộc tính cho Student
 
     // TODO: khai báo các phương thức getter, setter cho Student
+    private:
+        string  name,
+                group,
+                id,
+                email;
+    public:
+        String getName()  {return name;}
+        String getGroup() {return group;}
+        String getId()    {return id;}
+        String getEmail() {return email;}
+
+        void setName(String n){
+            this.name = n;
+        }
+        void setGroup(String g){
+            this.group = g;
+        }
+        void setId(String i){
+            this.id = i;
+        }
+        void setEmail(String e){
+            this.email = e;
+        }
+
 
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        name  = "Student";
+        id    = "000";
+        group = "INT22041";
+        email = "uet@vnu.edu.vn"
+
     }
 
     /**
@@ -21,6 +50,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name = n;
+        id   = sid;
+        email= em;
+        group = "INT22041";
     }
 
     /**
@@ -28,7 +61,12 @@ public class Student {
      * @param s
      */
     Student(Student s) {
+
         // TODO:
+        name = s.name;
+        id   = s.id;
+        group= s.group;
+        email= s.email;
     }
 
     String getInfo() {
