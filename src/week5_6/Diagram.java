f36f5c575f08c256c8f8d1a086a6e980143e5097
habac99 <havanbac99@gmail.com>
2018-10-20 01:28:19 +0700

    update week 5

--- src/week5_6/Diagram.java
+++ src/week5_6/Diagram.java
@@ -7,12 +7,28 @@ import java.util.ArrayList;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Diagram  {
+    public void removeCircle(ArrayList<Shape> layer){
+        int i ;
+        for(i=0;i<layer.size();i++){
+
+            if(layer.get(i) instanceof Circle){
+                layer.remove(i);
+                i--;
+            }
+        }
+
+    }
     public static void main(String[] a){
 
         JFrame mainFrame = new JFrame("Week 5");
         Layer layer = new Layer();
         Layer layer2 = new Layer();
+        System.out.println("Số hình ban đầu là :"  + layer.layer.size());
         layer.removeTriangle();
+        System.out.println("Số hình sau khi xoá tam giác là: " +  layer.layer.size());
+
+
+
         mainFrame.add(layer);
 
         mainFrame.setVisible(true);
