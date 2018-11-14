87371472eab0cb4ecbf66fc2b249ecf655ff7acc
habac99 <havanbac99@gmail.com>
2018-11-14 14:24:28 +0700

    fg

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,13 @@
 package week10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public List<String> getAllFunction(String path){
+        List<String> function = new ArrayList<String>();
+        return null;
+
+
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,35 @@
 package week10;
 
+import java.util.Random;
+import java.math.*;
+
 public class Task2 {
+    public static void BubbleSort(){
+        double[] arr = new double[1000];
+        Random ran = new Random();
+        int i,j;
+        for(i=0;i<1000;i++){
+             arr[i] = ran.nextDouble() * 500;
+
+
+        }
+        double temp  = 0.0;
+        for(i = 0;i<999;i++){
+            for(j = 0;j<999 - i;j++){
+                if(arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        for(i=0;i<1000;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+    public  static void main(String[] args){
+        BubbleSort();
+    }
 }
